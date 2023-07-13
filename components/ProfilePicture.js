fdf12ae1d77050e99ae3e600023f2ae072741472
HojinSohn <112189065+HojinSohn@users.noreply.github.com>
2023-07-12 17:40:36 -0700

    Homepage edit style and format

--- components/ProfilePicture.js
+++ components/ProfilePicture.js
@@ -19,24 +19,4 @@ function ProfilePicture({ selectedImage, size}) {
     }
 }
 
-function ProfilePictureWithSize({ selectedImage, size }) {
-
-    if (selectedImage === null || selectedImage === undefined) {
-        return (
-            <Image
-                source={require('../assets/default_pfp.jpeg')}
-            />
-        )
-    } else {
-        // console.log("hello", selectedImage);
-        return (
-            <Image
-                source={{uri: selectedImage}} style={{ width: size, height: size , marginTop: 5}}
-                // source={{uri: selectedImage}}
-            />
-        )
-    }
-}
-
-
 export default ProfilePicture
